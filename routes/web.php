cb62a415fafe14fe9ca96c3334813f9980564e28
Biriii <bangeabar@gmail.com>
2025-10-08 17:39:19 +0700

    Add debug route for storage paths

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,14 @@ Route::get('/test-db', function () {
         return "❌ Lỗi: " . $e->getMessage();
     }
 });
+
+Route::get('/debug-storage', function () {
+    return [
+        'public_path_storage' => public_path('storage'),
+        'storage_path_app_public' => storage_path('app/public'),
+        'filesystems_config' => config('filesystems.links'),
+        'storage_exists' => file_exists(public_path('storage')),
+        'is_link' => is_link(public_path('storage')),
+        'readlink' => is_link(public_path('storage')) ? readlink(public_path('storage')) : 'not a link',
+    ];
+});
