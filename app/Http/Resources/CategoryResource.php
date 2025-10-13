5fa9c96ddba870beb5c26f2e29ad79030887b5fd
Biriii <bangeabar@gmail.com>
2025-10-14 00:06:57 +0700

    up

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -12,6 +12,7 @@ class CategoryResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
+            'products_count' => $this->whenCounted('products'),
             'created_at' => $this->created_at,
         ];
     }
